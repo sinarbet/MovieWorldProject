f81a27ed6c1bdd6ce643b0e956b844806b47ae22
Betül <betulsinar11@gmail.com>
2023-04-09 00:06:28 +0300

    feat: renamed project name

--- ios/MovieWorldProjectTests/MovieWorldProjectTests.m
+++ ios/MovieWorldProjectTests/MovieWorldProjectTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeProject2Tests : XCTestCase
+@interface MovieWorldProjectTests : XCTestCase
 
 @end
 
-@implementation AwesomeProject2Tests
+@implementation MovieWorldProjectTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
